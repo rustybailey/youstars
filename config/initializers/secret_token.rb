203d7044e9de141efbf5ab32356bd6cf4a3da2cb
Jason B <devilshaircut@gmail.com>
2013-08-31 17:38:44 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youstars::Application.config.secret_key_base = '3216fef1b2add3e50179ee31a66a082d2b07c7774eb762b967208835f6cea8d522450eb9cef6acb8a5d3e16f6938022a64107ef070ded9547fe8d831a36ce2e0'
