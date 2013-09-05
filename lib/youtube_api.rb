0ad1f68727336485fd2378d1fecb8ee62fe19b08
Mitchell Lane <mitchell@fullscreen.net>
2013-09-05 15:36:48 -0400

    Fixing up the api key situation

--- lib/youtube_api.rb
+++ lib/youtube_api.rb
@@ -20,7 +20,7 @@ module YoutubeApi
 
     channel_url = @@v3_URL + "/channels"
     query = {
-      key: $YOUTUBE_V3_API_KEY,
+      key: ENV['YOUTUBE_API'],
       id: channel_id,
       part: "contentDetails"
     }
@@ -34,7 +34,7 @@ module YoutubeApi
   def self.video_list_for_playlist(playlist_id, limit, page_token)
     playlist_url = @@v3_URL + "/playlistItems"
     query = {
-      key: $YOUTUBE_V3_API_KEY,
+      key: ENV['YOUTUBE_API'],
       playlistId: playlist_id,
       part: "contentDetails",
       maxResults: limit,
@@ -51,7 +51,7 @@ module YoutubeApi
   def self.video_data_for_video_id(video_id)
     video_url = @@v3_URL + "/videos"
     query = {
-      key: $YOUTUBE_V3_API_KEY,
+      key: ENV['YOUTUBE_API'],
       id: video_id,
       part: "snippet,statistics"
     }
@@ -69,7 +69,7 @@ module YoutubeApi
   def self.channel_stats_for_channel_id(channel_id)
     channel_url = @@v3_URL + '/channels'
     query = {
-      key:  $YOUTUBE_V3_API_KEY,
+      key:  ENV['YOUTUBE_API'],
       id:   channel_id,
       part: 'statistics'
     }
