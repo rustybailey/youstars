370ab2cc2efde200f8e02f0edc0282a8a828de48
Mitchell Lane <mitchell@fullscreen.net>
2013-09-05 18:12:22 -0400

    Expose things in routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,12 @@ Youstars::Application.routes.draw do
 
   namespace :suggest do
 
-    get '/channels',             :controller => 'channels', :action => 'user'
-    get '/channels/:youtube_id', :controller => 'channels', :action => 'channel'
+    get '/channels',                  :controller => 'channels', :action => 'user'
+    get '/channels/:youtube_id',      :controller => 'channels', :action => 'channel'
+
+    get '/videos',                    :controller => 'videos', :action => 'user'
+    get '/videos/most_watched',       :controller => 'videos', :action => 'most_watched'
+    get '/videos/recently_watched',   :controller => 'videos', :action => 'recently_watched'
 
   end
 
