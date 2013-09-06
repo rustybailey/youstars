5eae61e760bf3858112953410cfa054dca3a53a5
Daniel Powell <danielp@fullscreen.net>
2013-09-06 10:13:40 -0400

    move ratings to users

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,27 +3,8 @@ class Channel < ActiveRecord::Base
   has_and_belongs_to_many :topics
 
   has_many :videos
-  has_many :ratings
 
   validates :name, :uniqueness => true
   validates :youtube_id, :uniqueness => true
-
-  def rated_videos(score = nil)
-    Video.where(:id => ratings_relation.map(&:id))
-  end
-
-  def liked_videos(score = 3)
-    ratings_relation = ratings
-    ratings_relation = ratings_relation.where( ["score >= ?", score] ) unless score.nil?
-
-    Video.where(:id => ratings_relation.map(&:id))
-  end
-
-  def disliked_videos(score = 2)
-    ratings_relation = ratings
-    ratings_relation = ratings_relation.where( ["score <= ?", score] ) unless score.nil?
-
-    Video.where(:id => ratings_relation.map(&:id))
-  end
 
 end
--- db/migrate/20130905224420_create_ratings.rb
+++ db/migrate/20130905224420_create_ratings.rb
@@ -2,7 +2,7 @@ class CreateRatings < ActiveRecord::Migration
 
   def change
     create_table :ratings do |t|
-      t.integer :channel_id
+      t.integer :user_id
       t.integer :video_id
       t.integer :score
     end
