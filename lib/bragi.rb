82c27e7fe78a3353d8b460412d61c8228761c6a1
Daniel Powell <danielp@fullscreen.net>
2013-09-06 15:23:48 -0400

    use bloom filters to record watch history for users

--- lib/bragi.rb
+++ lib/bragi.rb
@@ -4,29 +4,29 @@ module Bragi
   @@block_count = 2048
   @@k           = 2
 
-  def register_watch(user, video, block_width = @@block_width, block_count = @@block_count, k = @@k)
-    video_filter_name   = "#{user.guid}_video_filter"
-    channel_filter_name = "#{user.guid}_channel_filter"
+  def self.register_view(user_id, video_id, block_width = @@block_width, block_count = @@block_count, k = @@k)
+    video_filter_name   = "#{user_id}_video_filter"
+    channel_filter_name = "#{user_id}_channel_filter"
 
     video_filter   = Bloomfilter.new(video_filter_name,   block_width, block_count, k)
     channel_filter = Bloomfilter.new(channel_filter_name, block_width, block_count, k)
 
-    video_filter.add_element(video.youtube_id)
-    channel_filter.add_element(YoutubeApi.video_data_for_video_id(video.youtube_id)[:channel_id])    
+    video_filter.add_element(video_id)
+    channel_filter.add_element(YoutubeApi.video_data_for_video_id(video_id)[:channel_id])    
   end
 
-  def test_video(user, video, block_width = @@block_width, block_count = @@block_count, k = @@k)
-    video_filter_name = "#{user.guid}_video_filter"
+  def self.test_video(user_id, video_id, block_width = @@block_width, block_count = @@block_count, k = @@k)
+    video_filter_name = "#{user_id}_video_filter"
     video_filter      = Bloomfilter.new(video_filter_name, block_width, block_count, k)
     
-    video_filter.test_element(video.youtube_id)
+    video_filter.test_element(video_id)
   end
 
-  def test_channel(user, channel, block_width = @@block_width, block_count = @@block_count, k = @@k)
-    channel_filter_name = "#{user.guid}_channel_filter"
+  def self.test_channel(user_id, channel_id, block_width = @@block_width, block_count = @@block_count, k = @@k)
+    channel_filter_name = "#{user_id}_channel_filter"
     channel_filter      = Bloomfilter.new(channel_filter_name, block_width, block_count, k)
 
-    channel_filter.test_element(channel.youtube_id)
+    channel_filter.test_element(channel_id)
   end
 
 end
