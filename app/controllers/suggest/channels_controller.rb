d5715e62eeda14032713083b4f5d7dfc364e49af
Mitchell Lane <mitchell@fullscreen.net>
2013-09-06 21:14:40 -0400

    Bugfix

--- app/controllers/suggest/channels_controller.rb
+++ app/controllers/suggest/channels_controller.rb
@@ -22,7 +22,7 @@ class Suggest::ChannelsController < ApiController
       entry.dig("author", 0, "yt$userID", "$t")
     end
     
-    recs = recs.reject { |r| Bragi.test_channel(current_user.guid, r }.collect do |id|
+    recs = recs.reject { |r| Bragi.test_channel(current_user.guid, r ) }.collect do |id|
       YoutubeApi.channel_data_for_channel_id(id)
     end
 
