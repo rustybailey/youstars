395da0ca9c4aec6390fbda7b99dd880a79f4e40d
Mitchell Lane <mitchell@fullscreen.net>
2013-09-07 16:49:32 -0400

    Bugfixing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907015021) do
+ActiveRecord::Schema.define(version: 20130907203601) do
 
   create_table "categories", force: true do |t|
     t.integer  "youtube_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20130907015021) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "channels_topics", id: false, force: true do |t|
