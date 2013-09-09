beb190d83c1dab07df581e86bcc2b1c9d838edd2
Mitchell Lane <mitchell@fullscreen.net>
2013-09-08 22:19:32 -0400

    shorten

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Youstars::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local        = false
   config.action_controller.perform_caching  = true
-  config.static_cache_control               = "public, max-age=#{1.year.to_i}"
+  config.static_cache_control               = "public, max-age=#{1.hour.to_i}"
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
